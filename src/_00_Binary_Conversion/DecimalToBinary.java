1826c6ddc1912495266a57f6263db688e922e519
dave <dave@DESKTOP-5BF6RM1>
2020-07-27 09:51:42 -0700

    ip

--- src/_00_Binary_Conversion/DecimalToBinary.java
+++ src/_00_Binary_Conversion/DecimalToBinary.java
@@ -56,7 +56,7 @@ public class DecimalToBinary {
 		 * 
 		*/
 		
-		System.out.println( convertDecimalToBinary( 43 ) );
+		System.out.println( convertDecimalToBinary( 45 ) );
 	}
 	
     public static String convertDecimalToBinary(int decimalNum) {
